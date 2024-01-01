484b60aae494fd287bcdd4802e6056a223876eba
Michael Lohmann <mi.al.lohmann@gmail.com>
2024-01-02 00:17:43 +0100

    Add switch flag

--- test.sh
+++ test.sh
@@ -19,13 +19,19 @@ echo 'CommitMirInsAbendteuerland in new commit message'
 git commit -m 'My first commit' --quiet
 git show | grep --quiet "Flag: CommitMirInsAbendteuerland"
 
-echo 'ShowMeMore in branch commit'
-exec $(sed -n '/^```sh$/,/^```$/{n;p;}' commit.md) | grep --quiet "Flag: ShowMeMore"
-
 echo 'LocalCodeExecution flag should be deleted after execution of any hook (in this case the commit)'
 if ! [[ $(grep -L "# Flag: LocalCodeExecution" .git/hooks/*) ]]; then
     error "Selfdeleting flag was not removed after execution..."
     exit 1
 fi
+
+echo 'restore should not show Switcheridoo flag'
+git restore README.md 2>&1 | grep --quiet "Flag: Switcheridoo" && exit 1 || :
+
+echo 'ShowMeMore in branch commit'
+exec $(sed -n '/^```sh$/,/^```$/{n;p;}' commit.md) | grep --quiet "Flag: ShowMeMore"
+
+echo 'Switcheridoo when switching to "branches-explained"'
+git switch branches-explained 2>&1 | grep --quiet "Flag: Switcheridoo"
 
 echo success!
