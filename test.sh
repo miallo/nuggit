7de6d05abb16e772dc675914a4aa3348282929bc
Michael Lohmann <mi.al.lohmann@gmail.com>
2024-01-01 03:50:32 +0100

    add uncommitted `diff` flags
    Flags for plain `git diff` and `git diff --staged`

--- test.sh
+++ test.sh
@@ -6,12 +6,18 @@ set -e
 
 cd challenge
 
+echo 'WorkInProgress in diff'
+git diff | grep --quiet "Flag: WorkInProgress"
+
+echo 'CommitmentIssues in diff --staged'
+git diff --staged | grep --quiet "Flag: CommitmentIssues"
+
 echo 'LocalCodeExecution flag in hooks'
 grep --quiet "# Flag: LocalCodeExecution" .git/hooks/pre-auto-gc
 grep --quiet "^exit 1" .git/hooks/pre-auto-gc
 echo 'LocalCodeExecution flag should be deleted after execution of the hook'
 # git gc --auto # <- not executing the script?
-git hook run pre-auto-gc
+git hook run pre-auto-gc || :
 if ! [[ $(grep -L "# Flag: LocalCodeExecution" .git/hooks/*) ]]; then
     error "Selfdeleting flag was not removed after execution..."
     exit 1
