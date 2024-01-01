fdb48f0943f4ad64c202d8470d5ed28f866ab192
Michael Lohmann <mi.al.lohmann@gmail.com>
2024-01-01 14:45:40 +0100

    test.sh: run pre-auto-gc hook manually
    This is done, since `git hook` was only introduced recently. Instead of
    running this manually we should figure out how to trigger the gc...

--- test.sh
+++ test.sh
@@ -17,7 +17,8 @@ grep --quiet "# Flag: LocalCodeExecution" .git/hooks/pre-auto-gc
 grep --quiet "^exit 1" .git/hooks/pre-auto-gc
 echo 'LocalCodeExecution flag should be deleted after execution of the hook'
 # git gc --auto # <- not executing the script?
-git hook run pre-auto-gc || :
+# git hook run pre-auto-gc || : # cannot use since only introduced with git 2.40.0, which is e.g. not included in Ubuntu LTS
+./.git/hooks/pre-auto-gc || :
 if ! [[ $(grep -L "# Flag: LocalCodeExecution" .git/hooks/*) ]]; then
     error "Selfdeleting flag was not removed after execution..."
     exit 1
