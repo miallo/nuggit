5013b724984efba1e598ca18f493ad6fc10ca53a
Michael Lohmann <mi.al.lohmann@gmail.com>
2024-01-06 09:43:04 +0100

    test-lib/failure: print newline as first character
    This is done not to overwrite potential information, or if it was called
    directly after the `printf running` (without a newline at the end)

--- lib-test.sh
+++ lib-test.sh
@@ -6,7 +6,7 @@ success() {
 }
 
 failure() {
-    printf "❌ \e[3;1;31m%s\e[0m\e[1;31m failed\e[0m\n" "$1" >&2
+    printf "\n❌ \e[3;1;31m%s\e[0m\e[1;31m failed\e[0m" "$1" >&2
     trap - ERR EXIT # Remove the trap handler, so that it does not call itself
     exit 1
 }
