5ea86d37abce124162109f684e3fcca1fd03cd07
Michael Lohmann <mi.al.lohmann@gmail.com>
2024-01-16 16:43:46 +0100

    lib: move store_nuggits into a function instead of a script
    There is no need to do it in a separate script

--- build.sh
+++ build.sh
@@ -54,7 +54,7 @@ Have a free nuggit!" > .git/nuggits
 
 # TODO: once we have the origin and another "clone" in the .git folder, we should store the blobs in there, because it is trivial to list all of them with `git fsck --dangling | cut -d " " -f3 | xargs -n 1 git cat-file -p`
 # shellcheck disable=1091 # it does not seem to like the indirection
-. "$DOCDIR/store_nuggits.sh" # register the nuggits in our "git database" (aka some loose objects)
+store_nuggits
 ALMOST_CREDITS_HASH="$(git hash-object -w "$DOCDIR/almost_credits.txt")"
 # for the final credits do a little rot13, just to make life a bit harder if anyone e.g. greps through the loose objects...
 FINAL_CREDITS_HASH="$(tr 'A-Za-z' 'N-ZA-Mn-za-m' < "$DOCDIR/credits.txt" | git hash-object -w --stdin)"
