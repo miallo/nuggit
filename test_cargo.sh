cd8fde54a69285a91089daaa942950cfd2f67191
Michael Lohmann <git@lohmann.sh>
2025-10-21 18:21:01 +0200

    test: mark steps with commented tests

--- test_cargo.sh
+++ test_cargo.sh
@@ -97,6 +97,7 @@ expect "GIT_EDITOR="cat" git commit" to succeed
 expect "git show" to contain "nuggit: BigCommitment"
 redeem_nuggit BigCommitment
 '
+########################
 
 it 'chapter diff' '
 diff_command="$(get_sh_codeblock <(git log --pretty=%B @~...))"
@@ -141,6 +142,7 @@ it 'chapter working with branches' '
 expect "git switch branches-explained" error to contain "nuggit: Switcheridoo"
 redeem_nuggit Switcheridoo
 '
+################### 
 
 it 'MyFirstBranch when creating new branch' '
 expect "git switch -c my-new-branch" error to contain "nuggit: MyFirstBranch"
